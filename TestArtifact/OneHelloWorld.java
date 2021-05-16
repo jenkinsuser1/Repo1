b3b06a3344195a8376a6d8b3c4c25dfb66665ff2
Ira <Ira@LAPTOP-2BKA36GC>
2021-05-16 13:32:18 +0530

    commit 1 prashant

---
+++ TestArtifact/OneHelloWorld.java
@@ -0,0 +1,10 @@
+package Jan12;
+
+public class OneHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World by Technolamror");
+	}
+
+}
